5997f1dd265323883e95db48f55c54c23b5a2f68
Shad Campbell <shadc@gmx.com>
2014-03-07 11:42:55 -0800

    Update to font awesome.   Add help.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-Hands Free Map
+Speak2map
 ===========
 
-Hands Free Map allows you to speak commands into the microphone of your phone or PC to navigate and alter the map and web site.  Ideal for when you need your hands for other uses.
+Voice controlled map allows you to speak commands into the microphone of your phone or PC to navigate and alter the map and web site.  Ideal for when you need your hands for other uses.
 
-[Demo here](http://bit.ly/handsfreemap)
+[Demo here](http://bit.ly/Myhandsfreemap)
 
 Created By [shadcampbell](https://github.com/shadcampbell) [@shadc](https://twitter.com/shadc)
 
@@ -11,10 +11,11 @@ Created By [shadcampbell](https://github.com/shadcampbell) [@shadc](https://twit
 * [Bootstrap](http://getbootstrap.com)
 * [Bootstrap-map-js](https://github.com/Esri/bootstrap-map-js)
 * [jQuery](http://jquery.com/)
+* [Font Awesome](http://fortawesome.github.io/Font-Awesome/)
 * [Annyang SpeechRecognition](https://www.talater.com/annyang/)
 * [HTML5 - Speech API](https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html)
 
 
 ## Known Issues:
-* Limited browser support. 
+* Limited browser support. Works with Chrome on desktops and Android devices.
 
--- js/arcgis.js
+++ js/arcgis.js
@@ -7,13 +7,13 @@ require([
   "esri/symbols/Font", "esri/symbols/TextSymbol", "esri/geometry/Extent",
   "esri/geometry/webMercatorUtils", "dojo/_base/array", "dojo/_base/Color",
   "dojo/parser", "https://esri.github.io/bootstrap-map-js/src/js/bootstrapmap.js",  
-  "esri/arcgis/utils", "dojo/domReady!"
+  "dojo/domReady!"
 ], function (
   Map, Locator, SpatialReference, Graphic, SimpleLineSymbol, SimpleMarkerSymbol,
-  Font, TextSymbol, Extent, webMercatorUtils, arrayUtils, Color, parser, BootstrapMap, arcgisUtils
+  Font, TextSymbol, Extent, webMercatorUtils, arrayUtils, Color, parser, BootstrapMap
 ) {
     parser.parse();
-    map = new Map("mapDiv", { center: [-56.049, 38.485], zoom: 3, basemap: "streets" });
+    map = new Map("mapDiv", { center: [-56.049, 38.485], zoom: 3, basemap: "hybrid" });
     BootstrapMap.bindTo(map);
     locator = new Locator("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
     locator.on("address-to-locations-complete", function (evt) {
@@ -39,12 +39,9 @@ require([
         var esriExtent = new Extent(minx, miny, maxx, maxy, new SpatialReference({ wkid: 4326 }));
         map.setExtent(webMercatorUtils.geographicToWebMercator(esriExtent));
     });
-    function geoLocate(place) {
-        var address = { SingleLine: place };
-        var options = { address: address, outFields: ["*"] };
-        locator.addressToLocations(options);
-    }
+    //-- Wire up annyang 
     if (annyang) {
+        annyang.debug()
         $('#welcome').fadeIn('fast');
         var locate = function (place) { geoLocate(place); };
         var pan = function (type) {
@@ -75,7 +72,7 @@ require([
             'help': showHelp
         };
         annyang.addCallback('resultNoMatch', function () {
-            $('#nomatch').fadeIn('slow').delay(3000).fadeOut('slow'); $('#welcome').fadeOut('fast');
+            $('#nomatch').fadeIn('fast').delay(3000).fadeOut('slow'); $('#welcome').fadeOut('fast');
         });
         annyang.addCallback('resultMatch', function () { $('#welcome').fadeOut('fast'); });
         annyang.addCommands(commands);
@@ -83,6 +80,13 @@ require([
     } else {
         $('#notsupported').fadeIn('fast');
     }
+    //-- Helper functions
+    function geoLocate(place) {
+        var address = { SingleLine: place };
+        var options = { address: address, outFields: ["*"] };
+        locator.addressToLocations(options);
+    }
+    //-- Event Binding
     $(".dropdown-menu a").click(function () {
         map.setBasemap($(this).attr("data-basemapvalue"));
     });
@@ -90,7 +94,109 @@ require([
         geoLocate($("#search").val());
         e.preventDefault();
     });
+
+
+
+    function loadData() {
+        //var url = 'https://www.arcgis.com/sharing/rest/search?q=Terrestrial Ecoregions -type:"web mapping application" -type:"Layer Package" (type:"Web Map") &num=10&f=json';
+        var url = 'https://www.arcgis.com/sharing/rest/search?q=deschutes type:"Feature Service" &num=20&f=json';
+        $.getJSON(url, function (data) {
+
+            var furl = data.results[0].url.replace("http://", "https://") + "/0";
+            //$.getJSON(nurl, function (ndata) {
+            //    console.log(ndata);
+            //});
+
+            console.log(furl);
+            var featureLayer = new FeatureLayer(furl, {
+                mode: esri.layers.FeatureLayer.MODE_ONDEMAND
+            });
+            //map.addLayer(featureLayer);
+            console.log('1')
+            featureLayer.on("load", function () {
+                console.log(featureLayer.graphics);
+            });
+            console.log('2')
+
+            //dojo.connect(featureLayer, "load", function () {
+            //    console.log("loaded");
+            //    //var zoomExtent = esri.graphicsExtent(sessionLayer.graphics);
+            //    //map.setExtent(zoomExtent.expand(1.5));
+            //});
+
+
+            //var zoomExtent = esri.graphicsExtent(featureLayer.graphics);
+            //console.log(zoomExtent);
+
+            //var query = new esri.tasks.Query();
+            //query.geometry = map.extent;
+
+            ////console.log(featureLayer);
+
+            //featureLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (features) {
+
+            //    var g = new esri.layers.GraphicsLayer();
+
+            //    for (var i = 0, il = features.length; i < il; i++) {
+
+            //        var pt = features[i].geometry.getExtent().getCenter();
+            //        console.log(pt);
+            //        //var graphic = features[i].geometry;
+            //        g.add(new esri.Graphic(pt));
+            //        console.log(graphic);
+            //    }
+
+            //    console.log(g);
+
+            //    //var extent = esri.graphicsExtent(features);
+            //    //console.log(extent);
+            //    //console.log(features);
+            //    //var roomExtent = features[0].geometry.getExtent();
+            //    //console.log(roomExtent);
+            //    //map.setExtent(roomExtent);
+
+            //});
+
+            //x = featureLayer.getSelectedFeatures();
+            //console.log(x);
+
+            //var ex = new esri.geometry.Extent(featureLayer.initialExtent, featureLayer.SpatialReference);
+            //console.log(ex);
+
+            //var find = new esri.tasks.FindTask("https://services.arcgis.com/VYsrLd1WbPJ93eyz/ArcGIS/rest/services/OR_Deschutes_SalesActivity/FeatureServer");
+            //var params = new esri.tasks.FindParameters();
+            //params.layerIds = [0];
+            //params.searchFields = ["TitleCompa"];
+            //params.searchText = "WESTERN TITLE & ESCROW CO";
+            //find.execute(params, function () {
+            //    console.log("OK");
+            //});
+
+            //var findTask = new esri.tasks.FindTask(url);
+            //var findParams = new esri.tasks.FindParameters();
+            //findParams.returnGeometry = true;
+            ////findParams.layerIds = [0];
+            ////findParams.searchFields = ["RoomName"];
+            ////findParams.searchText = room;
+            //findTask.execute(findParams, function (results) {
+            //    console.log(results);
+            //});
+
+
+            //console.log(map.extent);
+            //console.log(featureLayer.initialExtent)
+            //map.setExtent(featureLayer.initialExtent);
+            //setWebMap(data.results[0].id);
+        });
+
+    }
+
 });
+
+
+
+
+
 
 
 
